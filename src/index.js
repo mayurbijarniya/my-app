bb1b6f3d35d6178629eda9d7d4ee10da5ddc6436
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-02-23 10:40:56 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+const myElement = React.createElement(
+  "ul",
+  {},
+  React.createElement("li", { style: { color: "blue" } }, "1 lb Salmon"),
+  React.createElement("li", { style: { color: "blue" } }, "1 cup Pine Nuts"),
+  React.createElement(
+    "li",
+    { style: { color: "blue" } },
+    "2 cup Butter Lettuce"
+  ),
+  React.createElement("li", { style: { color: "blue" } }, "1/2 cup olive oil"),
+  React.createElement("li", { style: { color: "blue" } }, "3 cloves of Garlic")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(myElement);
