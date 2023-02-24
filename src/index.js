ce8ef500b5d00dce64697d4b337ace5e0773fa81
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-02-24 13:15:10 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -16,20 +16,29 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const myElement = React.createElement(
+//   "ul",
+//   {},
+//   React.createElement("li", { style: { color: "blue" } }, "1 lb Salmon"),
+//   React.createElement("li", { style: { color: "blue" } }, "1 cup Pine Nuts"),
+//   React.createElement(
+//     "li",
+//     { style: { color: "blue" } },
+//     "2 cup Butter Lettuce"
+//   ),
+//   React.createElement("li", { style: { color: "blue" } }, "1/2 cup olive oil"),
+//   React.createElement("li", { style: { color: "blue" } }, "3 cloves of Garlic")
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(myElement);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-const myElement = React.createElement(
-  "ul",
-  {},
-  React.createElement("li", { style: { color: "blue" } }, "1 lb Salmon"),
-  React.createElement("li", { style: { color: "blue" } }, "1 cup Pine Nuts"),
-  React.createElement(
-    "li",
-    { style: { color: "blue" } },
-    "2 cup Butter Lettuce"
-  ),
-  React.createElement("li", { style: { color: "blue" } }, "1/2 cup olive oil"),
-  React.createElement("li", { style: { color: "blue" } }, "3 cloves of Garlic")
-);
+const myElement = <h1>hello</h1>;
+<ul>
+  <li>1cup tea</li>
+</ul>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(myElement);
