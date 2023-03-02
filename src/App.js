876f4ae5def30419b153f2c95c438682c3efd207
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-03-02 10:44:30 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 //import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
-}
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+// export default App;
+
+// import React from "react";
+// import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
+// function NavigationBar() {
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <Container>
+//         <Navbar.Brand href="#home">My Website</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#about">About</Nav.Link>
+//             <NavDropdown title="Services" id="basic-nav-dropdown">
+//               <NavDropdown.Item href="#service1">Service 1</NavDropdown.Item>
+//               <NavDropdown.Item href="#service2">Service 2</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#all-services">
+//                 All Services
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -34,11 +34,108 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(myElement);
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// function Book() {
+//   return <h2>Hello, I am a book</h2>;
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Book />);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// function Profession() {
+//   return <h2>Hello, I am a student</h2>;
+// }
+// function Intro() {
+//   return (
+//     <>
+//       <h1>My name is Mayur</h1>
+//       <Profession />
+//     </>
+//   );
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Intro />);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// function Book(props) {
+//   return <h2>The book is {props.name}</h2>;
+// }
+// const myElement = <Book name="Fundamentals of React Js" />;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(myElement);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// function Book(props) {
+//   return (
+//     <>
+//       <div
+//         style={{
+//           backgroundColor: "blue",
+//           width: "100px",
+//           height: "100px",
+//         }}
+//       ></div>
+//       <h2 style={{ color: "red" }}>The book is {props.name}</h2>;
+//     </>
+//   );
+// }
+// function Bag() {
+//   return (
+//     <>
+//       <h1>Which book is there in the bag?</h1>
+//       <Book name="Fundamentals of React Js" />
+//     </>
+//   );
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Bag />);
+
+// function Bags(props) {
+//   return (
+//     <div style={{ border: "solid red 1px", background: "yellow" }}>
+//       <h2 style={{ color: "green" }}>The book is {props.name}</h2>
+//     </div>
+//   );
+// }
+
+// function Book(props) {
+//   const author = " wriiten by niteesh";
+//   return (
+//     <>
+//       <h1 style={{ color: "red" }}>which book is this </h1>
+//       <Bags name={author} />
+//     </>
+//   );
+// }
+
+// // const myElement = <Book name = "fundamental of React js"/>;
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Book />);
 import React from "react";
 import ReactDOM from "react-dom/client";
-const myElement = <h1>hello</h1>;
-<ul>
-  <li>1cup tea</li>
-</ul>;
+import "./NavigationBar.css";
+
+function NavigationBar() {
+  return (
+    <nav className="navigation-bar">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(myElement);
+root.render(<NavigationBar />);
