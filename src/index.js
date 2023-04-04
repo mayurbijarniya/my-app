54d425f3ca8699351732ebdaba1b568fd237eb33
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-04-04 14:22:13 +0530

    fff

--- src/index.js
+++ src/index.js
@@ -140,3 +140,196 @@
 // }
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<NavigationBar />);
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// class ControlledComponent extends React.Component {
+//   state = {
+//     username: "",
+//     password: "",
+//   };
+//   handleUsernameChange = (e) =>
+//     this.setState({
+//       username: e.target.value,
+//     });
+//   handlePasswordChange = (e) =>
+//     this.setState({
+//       password: e.target.value,
+//     });
+//   handleSubmit = (e) => {
+//     alert(
+//       "The details are: " + this.state.username + " and" + this.state.password
+//     );
+//     e.preventDefault();
+//   };
+//   render() {
+//     return (
+// <form onSubmit={this.handleSubmit}>
+//   Username:
+//   <input
+//     type={"text"}
+//     value={this.state.username}
+//     onChange={this.handleUsernameChange}
+//   />
+//   Password:{" "}
+//   <input
+//     type={"text"}
+//     value={this.state.password}
+//     onChange={this.handlePasswordChange}
+//   />
+//   <input type="submit" />
+// </form>
+//     );
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<ControlledComponent />);
+// import React,{useState} from 'react';
+// import ReactDOM from "react-dom/client";
+// const ControlledComponentWithHooks = () => {
+//   const [state, setState] = useState({username:'',password:''})
+//   const handleInputChange = (e) => {
+//     const{name,value} = e.target;
+//     setState((prevProps) => ({
+//       ...prevProps,
+//       [name]:value
+//     }));
+//   };
+//   const handleSubmit = (e) => {
+//     alert('The details are: '+ state.username + " and " + state.password);
+//     e.preventDEfault();
+//   }
+//   return {
+//     <form  onsubmit = {handleSubmit}>
+//         Username:<input type={"text"} name="username" value={state.username} onChange={handleInputChange}/>
+//         Password:
+//         <input
+//           type={"text"}
+//           name="password"
+//           value={state.password}
+//           onChange={handleInputChange}
+//         />
+//         <input type="submit" />
+//       </form>
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<ControlledComponentWithHooks />);
+// import React, { useRef } from "react";
+// import ReactDOM from "react-dom/client";
+// const UncontrolledComponent = () => {
+//   const inputRef1 = useRef(null);
+//   const inputRef2 = useRef(null);
+//   const handleSubmit = (e) => {
+//     alert(
+//       "the details are : " +
+//         inputRef1.current.value +
+//         "and " +
+//         inputRef2.current.value
+//     );
+//     e.preventDefault();
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       Username:
+//       <input type="text" ref={inputRef1} />
+//       Password:
+//       <input type="text" ref={inputRef2} />
+//       <input type="submit" />
+//     </form>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<UncontrolledComponent />);
+// import React, { useState } from "react";
+// import axios from "axios";
+// import ReactDOM from "react-dom/client";
+// const App = () => {
+//   const [users, setUsers] = useState([]);
+//   axios
+//     .get("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => setUsers(response.data))
+//     .catch((e) => console.log("something went wrong : (", e));
+//   return (
+//     <div>
+//       <ul>
+//         {users.map((user) => (
+//           <li>
+//             {" "}
+//             key={user.id}>{user.id}-{user.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+import React, { useState } from "react";
+import axios from "axios";
+import ReactDOM from "react-dom/client";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post(
+        "https://jsonplaceholder.typicode.com/users",
+        formData,
+        config
+      );
+      console.log(res.data); // show response data in console
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={name}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Form />);
